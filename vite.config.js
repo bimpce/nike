2153b41b1a78f7712364cb039aa520af72d07475
bimp <bimp.ce@gmail.com>
2023-09-07 19:13:50 +0200

    https___

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://bimpce.github.io/nike',
+  base: 'https://bimpce.github.io/nike',
   plugins: [react()],
 });
